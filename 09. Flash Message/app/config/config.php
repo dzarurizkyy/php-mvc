6d0d6acc8069272c74e511b724e3ca5c3bfe768c
dzarurizkyy <dzarurizkybusiness@gmail.com>
2024-08-21 01:22:01 +0700

    finish material 9 : flash message

---
+++ 09. Flash Message/app/config/config.php
@@ -0,0 +1,7 @@
+<?php  
+  define("BASEURL", "http://localhost/php-mvc/09.%20Flash%20Message/public");
+  define("DBHOST", "localhost");
+  define("DBUSER", "root");
+  define("DBPASS", "");
+  define("DBNAME", "php_basic");
+?>
---
+++ 09. Flash Message/public/index.php
@@ -0,0 +1,6 @@
+<?php
+  // Start a session if one doesn't already exist
+  if (!session_id()) session_start();
+  
+  require_once "../app/init.php"; 
+?>
